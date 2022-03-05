ee4d4d081505d0c49006c66118f39944a5ab7745
Adaeze Ndupu <andupu4@gmail.com>
2022-03-05 02:26:58 +0100

    Convert image to .webp

--- js/main.js
+++ js/main.js
@@ -1,4 +1,216 @@
-const scrollElements=document.querySelectorAll('.js-scroll');const elementInView=(el,dividend=1)=>{const elementTop=el.getBoundingClientRect().top;return(elementTop<=(window.innerHeight||document.documentElement.clientHeight)/dividend)};const elementOutofView=(el)=>{const elementTop=el.getBoundingClientRect().top;return(elementTop>(window.innerHeight||document.documentElement.clientHeight))};const displayScrollElement=(element)=>{element.classList.add('scrolled')};const hideScrollElement=(element)=>{element.classList.remove('scrolled')};const handleScrollAnimation=()=>{scrollElements.forEach((el)=>{if(elementInView(el,1.25)){displayScrollElement(el)}else if(elementOutofView(el)){hideScrollElement(el)}})};window.addEventListener('scroll',()=>{handleScrollAnimation()});(function($){'use strict';var cfg={scrollDuration:200,},$WIN=$(window);var doc=document.documentElement;doc.setAttribute('data-useragent',navigator.userAgent);var ssPreloader=function(){$('html').addClass('ss-preload');$WIN.on('load',function(){$('#loader').fadeOut('slow',function(){$('#preloader').delay(100).fadeOut('slow')});$('html').removeClass('ss-preload');$('html').addClass('ss-loaded')})};var ssMoveHeader=function(){var hero=$('.page-hero'),hdr=$('header'),triggerHeight=hero.outerHeight()-170;$WIN.on('scroll',function(){var loc=$WIN.scrollTop();if(loc>triggerHeight){hdr.addClass('sticky')}else{hdr.removeClass('sticky')}
-if(loc>triggerHeight+20){hdr.addClass('offset')}else{hdr.removeClass('offset')}
-if(loc>triggerHeight+150){hdr.addClass('scrolling')}else{hdr.removeClass('scrolling')}})};var ssMobileMenu=function(){var toggleButton=$('.header-menu-toggle'),nav=$('.header-nav-wrap');toggleButton.on('click',function(event){event.preventDefault();toggleButton.toggleClass('is-clicked');nav.slideToggle()});if(toggleButton.is(':visible'))nav.addClass('mobile');$WIN.on('resize',function(){if(toggleButton.is(':visible'))nav.addClass('mobile');else nav.removeClass('mobile')});nav.find('a').on('click',function(){if(nav.hasClass('mobile')){toggleButton.toggleClass('is-clicked');nav.slideToggle()}})};var ssWaypoints=function(){var sections=$('.target-section'),navigation_links=$('.header-nav li a');sections.waypoint({handler:function(direction){var active_section;active_section=$('section#'+this.element.id);if(direction==='up')
-active_section=active_section.prevAll('.target-section').first();var active_link=$('.header-nav li a[href="#'+active_section.attr('id')+'"]');navigation_links.parent().removeClass('current');active_link.parent().addClass('current')},offset:'25%',})};var ssSmoothScroll=function(){$('.smoothscroll').on('click',function(e){var target=this.hash,$target=$(target);e.preventDefault();e.stopPropagation();$('html, body').stop().animate({scrollTop:$target.offset().top,},cfg.scrollDuration,'swing',function(){window.location.hash=target})})};var ssBackToTop=function(){var pxShow=500,fadeInTime=300,fadeOutTime=300,scrollSpeed=300,goTopButton=$('.go-top');$(window).on('scroll',function(){if($(window).scrollTop()>=pxShow){goTopButton.fadeIn(fadeInTime)}else{goTopButton.fadeOut(fadeOutTime)}})};(function ssInit(){ssPreloader();ssMoveHeader();ssMobileMenu();ssWaypoints();ssSmoothScroll();ssBackToTop()})()})(jQuery)
+//Scroll Animations
+const scrollElements = document.querySelectorAll('.js-scroll');
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add('scrolled');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('scrolled');
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
+
+(function ($) {
+  'use strict';
+
+  var cfg = {
+      scrollDuration: 200, // smoothscroll duration
+    },
+    $WIN = $(window);
+
+  // Add the User Agent to the <html>
+  // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+  var doc = document.documentElement;
+  doc.setAttribute('data-useragent', navigator.userAgent);
+
+  /* Preloader
+   * -------------------------------------------------- */
+  var ssPreloader = function () {
+    $('html').addClass('ss-preload');
+
+    $WIN.on('load', function () {
+      // force page scroll position to top at page refresh
+      // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+      // will first fade out the loading animation
+      $('#loader').fadeOut('slow', function () {
+        // will fade out the whole DIV that covers the website.
+        $('#preloader').delay(100).fadeOut('slow');
+      });
+
+      // for hero content animations
+      $('html').removeClass('ss-preload');
+      $('html').addClass('ss-loaded');
+    });
+  };
+
+  /* Move header
+   * -------------------------------------------------- */
+  var ssMoveHeader = function () {
+    var hero = $('.page-hero'),
+      hdr = $('header'),
+      triggerHeight = hero.outerHeight() - 170;
+
+    $WIN.on('scroll', function () {
+      var loc = $WIN.scrollTop();
+
+      if (loc > triggerHeight) {
+        hdr.addClass('sticky');
+      } else {
+        hdr.removeClass('sticky');
+      }
+
+      if (loc > triggerHeight + 20) {
+        hdr.addClass('offset');
+      } else {
+        hdr.removeClass('offset');
+      }
+
+      if (loc > triggerHeight + 150) {
+        hdr.addClass('scrolling');
+      } else {
+        hdr.removeClass('scrolling');
+      }
+    });
+  };
+
+  /* Mobile Menu
+   * ---------------------------------------------------- */
+  var ssMobileMenu = function () {
+    var toggleButton = $('.header-menu-toggle'),
+      nav = $('.header-nav-wrap');
+
+    toggleButton.on('click', function (event) {
+      event.preventDefault();
+
+      toggleButton.toggleClass('is-clicked');
+      nav.slideToggle();
+    });
+
+    if (toggleButton.is(':visible')) nav.addClass('mobile');
+
+    $WIN.on('resize', function () {
+      if (toggleButton.is(':visible')) nav.addClass('mobile');
+      else nav.removeClass('mobile');
+    });
+
+    nav.find('a').on('click', function () {
+      if (nav.hasClass('mobile')) {
+        toggleButton.toggleClass('is-clicked');
+        nav.slideToggle();
+      }
+    });
+  };
+
+  /* Highlight the current section in the navigation bar
+   * ------------------------------------------------------ */
+  var ssWaypoints = function () {
+    var sections = $('.target-section'),
+      navigation_links = $('.header-nav li a');
+
+    sections.waypoint({
+      handler: function (direction) {
+        var active_section;
+
+        active_section = $('section#' + this.element.id);
+
+        if (direction === 'up')
+          active_section = active_section.prevAll('.target-section').first();
+
+        var active_link = $(
+          '.header-nav li a[href="#' + active_section.attr('id') + '"]'
+        );
+
+        navigation_links.parent().removeClass('current');
+        active_link.parent().addClass('current');
+      },
+
+      offset: '25%',
+    });
+  };
+
+  /* Smooth Scrolling
+   * ------------------------------------------------------ */
+  var ssSmoothScroll = function () {
+    $('.smoothscroll').on('click', function (e) {
+      var target = this.hash,
+        $target = $(target);
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: $target.offset().top,
+          },
+          cfg.scrollDuration,
+          'swing',
+          function () {
+            window.location.hash = target;
+          }
+        );
+    });
+  };
+
+  /* Back to Top
+   * ------------------------------------------------------ */
+  var ssBackToTop = function () {
+    var pxShow = 500, // height on which the button will show
+      fadeInTime = 300, // how slow/fast you want the button to show
+      fadeOutTime = 300, // how slow/fast you want the button to hide
+      scrollSpeed = 300, // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+      goTopButton = $('.go-top');
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function () {
+      if ($(window).scrollTop() >= pxShow) {
+        goTopButton.fadeIn(fadeInTime);
+      } else {
+        goTopButton.fadeOut(fadeOutTime);
+      }
+    });
+  };
+
+  /* Initialize
+   * ------------------------------------------------------ */
+  (function ssInit() {
+    ssPreloader();
+    ssMoveHeader();
+    ssMobileMenu();
+    ssWaypoints();
+    ssSmoothScroll();
+    ssBackToTop();
+  })();
+})(jQuery);
